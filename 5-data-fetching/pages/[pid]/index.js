aa58c68052cdcb12931d4923339813ce7750faa0
afifputra <afifputraharyono@gmail.com>
2022-10-12 11:32:13 +0700

    pre-render => getStaticPaths dynamically

--- 5-data-fetching/pages/[pid]/index.js
+++ 5-data-fetching/pages/[pid]/index.js
@@ -4,6 +4,10 @@ import fs from "fs/promises";
 const ProductDetailPage = (props) => {
   const { loadedProduct } = props;
 
+  if (!loadedProduct || loadedProduct.length === 0) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
       <h1>{loadedProduct.title}</h1>
@@ -12,16 +16,19 @@ const ProductDetailPage = (props) => {
   );
 };
 
-export const getStaticProps = async (context) => {
-  console.log(context);
-
-  const { params } = context;
-  const { pid } = params;
-
+const getData = async () => {
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  return data;
+};
+
+export const getStaticProps = async (context) => {
+  const { params } = context;
+  const { pid } = params;
+
+  const data = await getData();
   const product = data.products.find((product) => product.id === pid);
 
   if (!product) {
@@ -39,25 +46,14 @@ export const getStaticProps = async (context) => {
 };
 
 export const getStaticPaths = async () => {
+  const data = await getData();
+  const ids = data.products.map((product) => product.id);
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+  console.log(pathsWithParams);
+
   return {
-    paths: [
-      {
-        params: {
-          pid: "p1",
-        },
-      },
-      {
-        params: {
-          pid: "p2",
-        },
-      },
-      {
-        params: {
-          pid: "p3",
-        },
-      },
-    ],
-    fallback: false,
+    paths: pathsWithParams,
+    fallback: true,
   };
 };
 
