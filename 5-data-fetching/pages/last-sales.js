ced8612d5b612b2d96ec0e2d0d36f058873ab1ec
afifputra <afifputraharyono@gmail.com>
2022-10-13 11:02:39 +0700

    add hooks useSWR

--- 5-data-fetching/pages/last-sales.js
+++ 5-data-fetching/pages/last-sales.js
@@ -1,44 +1,75 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 const LastSalesPage = () => {
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    fetchSalesHandler();
-  }, []);
+  const { data, error } = useSWR("https://nextjs-course-92950-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json", fetcher);
 
-  async function fetchSalesHandler() {
-    setIsLoading(true);
-    const response = await fetch("https://nextjs-course-92950-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json");
-
+  async function fetcher(url) {
+    const response = await fetch(url);
     const data = await response.json();
-
-    if (!response.ok) {
-      throw new Error(data.message || "Something went wrong!");
-    }
-
-    const transformedSales = [];
-
-    for (const key in data) {
-      transformedSales.push({
-        id: key,
-        username: data[key].username,
-        volume: data[key].volume,
-      });
-    }
-
-    setSales(transformedSales);
-    setIsLoading(false);
+    return data;
   }
 
-  if (isLoading) {
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  // useEffect(() => {
+  //   fetchSalesHandler();
+  // }, []);
+
+  // async function fetchSalesHandler() {
+  //   setIsLoading(true);
+  //   const response = await fetch("https://nextjs-course-92950-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json");
+
+  //   const data = await response.json();
+
+  //   if (!response.ok) {
+  //     throw new Error(data.message || "Something went wrong!");
+  //   }
+
+  //   const transformedSales = [];
+
+  //   for (const key in data) {
+  //     transformedSales.push({
+  //       id: key,
+  //       username: data[key].username,
+  //       volume: data[key].volume,
+  //     });
+  //   }
+
+  //   setSales(transformedSales);
+  //   setIsLoading(false);
+  // }
+  if (error) {
+    return <p>Failed to load.</p>;
+  }
+
+  if (!data || !sales) {
     return <p>Loading...</p>;
   }
 
-  if (!sales) {
-    return <p>No data found.</p>;
-  }
+  // if (isLoading) {
+  //   return <p>Loading...</p>;
+  // }
+
+  // if (!sales) {
+  //   return <p>Failed to load.</p>;
+  // }
 
   return (
     <ul>
