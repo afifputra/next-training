0c6ed25d5e01797e5f2da200eb4465e24a5dce5c
afifputra <afifputraharyono@gmail.com>
2022-10-15 11:06:21 +0700

    add head to _app js

--- 7-optimizing-next/pages/_app.js
+++ 7-optimizing-next/pages/_app.js
@@ -1,9 +1,16 @@
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import Head from "next/head";
+
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>NextJS Events</title>
+        <meta name="description" content="Next JS Events" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
