61aacc49f86eee0bd102be429c1f52bfd102b7d4
afifputra <afifputraharyono@gmail.com>
2022-10-17 15:43:51 +0700

    add api routes dynamically

---
+++ 8-api-routes/pages/api/[feedbackId].js
@@ -0,0 +1,15 @@
+import { buildFeedbackPath, extractFeedback } from "./feedback.js";
+
+function handler(req, res) {
+  const { feedbackId } = req.query;
+  console.log(feedbackId);
+
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+
+  const selectedProduk = data.find((item) => item.id === feedbackId);
+
+  res.status(200).json({ feedback: selectedProduk });
+}
+
+export default handler;
--- 8-api-routes/pages/feedback/index.js
+++ 8-api-routes/pages/feedback/index.js
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { Fragment } from "react";
 import { extractFeedback, buildFeedbackPath } from "../api/feedback";
 
 function FeedbackPage(props) {
+  const [detailFeedback, setDetailFeedback] = useState();
+
+  const loadDetailHandler = async (id) => {
+    const response = await fetch(`/api/${id}`);
+    const data = await response.json();
+    setDetailFeedback(data.feedback);
+  };
+
   return (
     <Fragment>
+      {detailFeedback && <p>{detailFeedback.email}</p>}
       <ul>
         {!props.feedbackItems.length && <p>No feedback yet!</p>}
         {props.feedbackItems.map((item) => (
-          <li key={item.id}>{item.text}</li>
+          <li key={item.id}>
+            {item.text} <button onClick={loadDetailHandler.bind(null, item.id)}>Show Details</button>
+          </li>
         ))}
       </ul>
     </Fragment>
